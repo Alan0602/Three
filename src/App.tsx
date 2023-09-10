46463cbffc231ed258489179c32abbbd9c8e5d78
Alan0602 <alankanil1234@gmail.com>
2023-09-10 08:57:20 +0530

    feat():New saturn formed

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import ThreeScene from "./ThreeScene/ThreeScene"
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="App">
+      <header className="App-header">
+        <ThreeScene />
+      </header>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/ThreeScene/ThreeScene.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { TextureLoader } from 'three';
+import saturn from '../assets/2k_saturn_ring_alpha.png';
+
+
+
+const ThreeScene: React.FC = () => {
+  const sceneRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Create a Three.js scene
+    const scene = new THREE.Scene();
+
+    // Create a camera
+    const camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 4;
+
+    // Create a WebGLRenderer
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Attach the renderer to the HTML element
+    if (sceneRef.current) {
+      sceneRef.current.appendChild(renderer.domElement);
+    }
+
+    // Load the texture
+const textureLoader = new TextureLoader();
+const texture = textureLoader.load(saturn,);
+
+// Create a material with the texture
+
+
+    // Create a cube
+    const geometry = new THREE.SphereGeometry();
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    const sphere = new THREE.Mesh(geometry, material);
+    scene.add(sphere);
+
+    // Animation loop
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      sphere.rotation.x += 0.01;
+      sphere.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  }, []);
+
+  return <div ref={sceneRef} />;
+};
+
+export default ThreeScene;
